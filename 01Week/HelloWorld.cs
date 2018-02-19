764cd812d6ad3c611465e376aa930e8a67b94b9b
jamiefox <jamiefox.fox3@gmail.com>
2018-02-19 09:24:58 -0600

    added text to read Hello World

--- 01Week/HelloWorld.cs
+++ 01Week/HelloWorld.cs
@@ -1,12 +1,12 @@
-using System;
+﻿using System;
 
 public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello");
+        Console.WriteLine("Hello World");
 
-        // leave this command at the end so your program does not close automatically
+
         Console.ReadLine();
     }
 }
