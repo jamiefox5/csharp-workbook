061f0cb7dd77caf8ab253c3f74e9e23bf93b60f9
jamiefox <jamiefox.fox3@gmail.com>
2018-02-27 20:12:50 -0600

    piglatin game

--- 01Week/DataTypes.cs
+++ 01Week/DataTypes.cs
@@ -1,14 +1,49 @@
-using System;
-using System.IO;
+﻿using System;
 
-public class Program
+
+
+class DataTypes
 {
     public static void Main()
     {
-        // your code goes here
+        //bool isHuman = true;
+        //bool f = false;
+        decimal num = 9.99m;
+        decimal total = num * num;
+        string firstName = "Jamie";
+        string lastName = "Fox";
+        int age = 51;
+        string job = "Teacher";
+        string favoriteBand = "Maroon 5";
+        string favoriteSportsTeam = "Spurs";
 
 
-        // leave this command at the end so your program does not close automatically
+        Console.WriteLine("First Name: " + firstName);
+        Console.WriteLine("Last Name: " + lastName);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("job: " + job);
+        Console.WriteLine("Favorite Band: " + favoriteBand);
+        Console.WriteLine("Favorite Sports Team: " + favoriteSportsTeam);
+        Console.ReadLine();
+
+
+        int myInteger = (int)num;
+        Console.WriteLine("My Integer: " + myInteger);
         Console.ReadLine();
     }
+
+    /*public override bool Equals(object obj)
+     {
+         return base.Equals(obj);
+     }
+
+     public override int GetHashCode()
+     {
+         return base.GetHashCode();
+     }
+
+     public override string ToString()
+     {
+         return base.ToString();*/
+
 }
